d02ae719392cb9b6497c25a2b6ce17e96db7bfd5
Redeamed <kdavis928@yahoo.com>
2016-01-21 12:38:00 -0700

    Maze Generation + level Progression

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public static LevelManager instance;
+    public Vector3 playerStart;
+    public GameObject player;
+    MazeGenerator mazeGen;
+    public Vector2 initDimension;
+    public int perLevel = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Debug.Log("LevelManager already exists in object: " + instance.gameObject.name);
+            Destroy(this);
+        }
+
+        mazeGen = GetComponent<MazeGenerator>();
+        mazeGen.RestartMaze((int)initDimension.x, (int)initDimension.y);
+	}
+
+    public void RestartMaze()
+    {
+        initDimension += Vector2.one * perLevel;
+        mazeGen.RestartMaze((int)initDimension.x, (int)initDimension.y);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.transform.position = playerStart;
+    }
+}
---
+++ Assets/Scripts/Tiles/scrNodeEnd.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrNodeEnd : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine("RestartWorld", 5);
+        }
+    }
+    IEnumerator RestartWorld()
+    {
+
+        LevelManager.instance.RestartMaze();
+        return null;
+    }
+}
+
+
